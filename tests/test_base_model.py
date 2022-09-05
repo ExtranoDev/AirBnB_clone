fc2404f9b0ca674fd52d28351dc74aa7b8c09103
ExtranoDev <jeomade19@gmail.com>
2022-09-05 21:45:40 +0000

    Test for base_model

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,26 +1,39 @@
 #!/usr/bin/python3
+"""
+Program test BaseModel class
+"""
 import unittest
 from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
+    """
+    Base class test
+    """
     base_model = BaseModel()
-    base_model1 = BaseModel()
+
+
+    def test_save(self):
+        """Tests the save function
+        """
+        pre_updated_at = TestBaseModel.base_model.updated_at
+        TestBaseModel.base_model.save()
+        post_updated_at = TestBaseModel.base_model.updated_at
+        self.assertNotEqual(pre_updated_at, post_updated_at)
 
     def test_instance(self):
         self.assertIsInstance(TestBaseModel.base_model, BaseModel)
 
     def test_id_uniq(self):
-        self.assertNotEqual(TestBaseModel.base_model.id,
-                            TestBaseModel.base_model1.id)
+        self.assertNotEqual(TestBaseModel.base_model.id, BaseModel().id)
 
-    def test_id_string(self):
-        self.assertEqual(type(TestBaseModel.base_model.id), str)
+    def test__str__(self):
+        # self.assertEqual(TestBaseModel.base_model.__str__)
+        pass
 
     def test_to_dict(self):
-        self.assertNotEqual(TestBaseModel.base_model.to_dict(),
-                            TestBaseModel.base_model.__dict__)
+        self.assertNotEqual(TestBaseModel.base_model.to_dict(), BaseModel().__dict__)
 
     def test_created_at(self):
         self.assertNotEqual(TestBaseModel.base_model.created_at,
-                            TestBaseModel.base_model1.created_at)
+                            BaseModel().created_at)
