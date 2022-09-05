513613ed552a1e4d055ecd89eaffb224f6ef9045
ExtranoDev <jeomade19@gmail.com>
2022-09-05 19:59:07 +0000

    Test for base_model

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
-"""
-File contains code to test the BaseModel class
-"""
 import unittest
+from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
-    """
-    BaseModel tester class
-    """
-    pass
+    base_model = BaseModel()
+    base_model1 = BaseModel()
+
+    def test_instance(self):
+        self.assertIsInstance(TestBaseModel.base_model, BaseModel)
+
+    def test_id_uniq(self):
+        self.assertNotEqual(TestBaseModel.base_model.id,
+                            TestBaseModel.base_model1.id)
+
+    def test_id_string(self):
+        self.assertEqual(type(TestBaseModel.base_model.id), str)
+
+    def test_to_dict(self):
+        self.assertNotEqual(TestBaseModel.base_model.to_dict(),
+                            TestBaseModel.base_model.__dict__)
+
+    def test_created_at(self):
+        self.assertNotEqual(TestBaseModel.base_model.created_at,
+                            TestBaseModel.base_model1.created_at)
